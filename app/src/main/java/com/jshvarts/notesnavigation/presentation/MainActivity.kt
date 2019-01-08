eb4c75e889a5fa73922ca119e6ad4d03ef2dbfb5
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-01-08 14:39:36 +0300

    save

--- app/src/main/java/com/jshvarts/notesnavigation/presentation/MainActivity.kt
+++ app/src/main/java/com/jshvarts/notesnavigation/presentation/MainActivity.kt
@@ -17,35 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        setupNavigation()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navigateUp(findNavController(this, R.id.nav_host_fragment), drawerLayout)
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    private fun setupNavigation() {
-        val navController = findNavController(this, R.id.nav_host_fragment)
-
-        // Update action bar to reflect navigation
-        setupActionBarWithNavController(this, navController, drawerLayout)
-
-        // Handle nav drawer item clicks
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawerLayout.closeDrawers()
-            true
-        }
-
-        // Tie nav graph to items in nav drawer
-        setupWithNavController(navigationView, navController)
-    }
 }
