7a454a9a5494249bead7e647ccd97484a27dcef3
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-01-09 05:45:46 +0300

    save

---
+++ app/src/main/java/com/jshvarts/notesnavigation/presentation/search/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.jshvarts.notesnavigation.presentation.search
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.jshvarts.notesnavigation.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+}
+
+
